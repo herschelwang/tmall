f810b46a9244ff06fd6563c6c699021c5893b63c
heshun <2586625136@qq.com>
2018-04-19 09:46:10 +0800

    解决xml文件重复生成问题(未成功)

---
+++ src/main/java/name/heshun/tmall/util/MergeablePlugin.java
@@ -0,0 +1,33 @@
+package name.heshun.tmall.util;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * @Author: heshun
+ * @Date: Create in 2018/4/18 17:53
+ * @Content: 解决xml文件重复生成问题
+ */
+public class MergeablePlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        try {
+            Field field = sqlMap.getClass().getDeclaredField("isMergeable");
+            field.setAccessible(true);
+            field.setBoolean(sqlMap, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
